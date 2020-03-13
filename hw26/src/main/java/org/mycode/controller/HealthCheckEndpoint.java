ea1fc9009b07571af1256b1a984920af6c9ff0b6
TergBug <tergbug@gmail.com>
2020-03-13 15:13:11 +0200

    Initial commit (HW26)

--- hw26/src/main/java/org/mycode/controller/HealthCheckEndpoint.java
+++ hw26/src/main/java/org/mycode/controller/HealthCheckEndpoint.java
@@ -5,10 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
 import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
 import org.springframework.boot.actuate.health.Health;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-@PropertySource("application.properties")
 @Component
 @Endpoint(id = "healthCheck")
 public class HealthCheckEndpoint {
