28d28693f296d354b2abeefbbdbea7b107e31a47
TergBug <tergbug@gmail.com>
2020-04-01 00:24:11 +0300

    Initial commit (HW31)

--- hw31/src/main/java/org/mycode/repository/hibernate/DeveloperRepositoryImpl.java
+++ hw31/src/main/java/org/mycode/repository/hibernate/DeveloperRepositoryImpl.java
@@ -1,14 +1,10 @@
 package org.mycode.repository.hibernate;
 
 import org.hibernate.SessionFactory;
-import org.mycode.configs.DatabaseConfig;
 import org.mycode.exceptions.NoSuchEntryException;
 import org.mycode.model.Developer;
-import org.mycode.model.Skill;
-import org.mycode.repository.*;
+import org.mycode.repository.DeveloperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,37 +23,6 @@ public class DeveloperRepositoryImpl implements DeveloperRepository {
     @Autowired
     public DeveloperRepositoryImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DatabaseConfig.class);
-        DeveloperRepository developerRepository = applicationContext.getBean(DeveloperRepository.class);
-        AccountRepository accountRepository = applicationContext.getBean(AccountRepository.class);
-        ProjectRepository projectRepository = applicationContext.getBean(ProjectRepository.class);
-        SkillRepository skillRepository = applicationContext.getBean(SkillRepository.class);
-        CustomerRepository customerRepository = applicationContext.getBean(CustomerRepository.class);
-
-        skillRepository.create(new Skill(UUID.fromString("ad34f1da-43cd-4742-948c-f2e59a975839"), "Java"));
-
-//        customerRepository.create(new Customer("Cu8", Arrays.stream(
-//                new Project []{new Project("Pr1", Arrays.stream(
-//                        new Developer[]{developerRepository.getById(UUID.fromString("8F0B7DBA-89F5-42FD-9907-E9C2F6809C1C"))})
-//                        .collect(Collectors.toSet()), ProjectStatus.IMPLEMENTATION, null)})
-//                .collect(Collectors.toSet())));
-
-        //Developer developer = developerRepository.getById(UUID.fromString("9a4f7c3f-fe35-4b45-82f1-128b65cb3a92"));
-        //System.out.println(developer.getSkills());
-        //System.out.println(developer.getAccount());
-//        Account account = accountRepository.getById(UUID.fromString("e86937cd-1535-4c0e-9026-e6ae27adc992"));
-//        account.setStatus(AccountStatus.ACTIVE);
-//        developer.setAccount(account);
-        //developerRepository.update(developer);
-//        developerRepository.create(new Developer("P", "Empo", Arrays.stream(
-//                new Skill []{skillRepository.getById(UUID.fromString("77351156-9d20-405e-a0c5-24e17b4b0ad0")),})
-//                .collect(Collectors.toSet()),
-//                accountRepository.getById(UUID.fromString("77845a88-c171-4cd0-a73a-3045bdbe4f30"))));
-
-        //System.out.println("\n"+developerRepository.getById(UUID.fromString("9a4f7c3f-fe35-4b45-82f1-128b65cb3a92")).getAccount());
     }
 
     @Override
